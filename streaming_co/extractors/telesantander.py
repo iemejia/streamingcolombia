cdaeef64205e9591378b7cddac6863b07f917996
Ismaël Mejía <iemejia@gmail.com>
2013-11-16 21:54:43 +0100

    Fixed error in Tele Santander channel name

--- streaming_co/extractors/telesantander.py
+++ streaming_co/extractors/telesantander.py
@@ -6,7 +6,7 @@ from .staticextractor import StaticExtractor
 
 class TeleSantanderExtractor(StaticExtractor):
 
-    NAME = u'Canal 13'
+    NAME = u'Tele Santander'
     WEBPAGE_URL = 'http://www.telesantander.com/'
     LOGO_URL = 'http://www.telesantander.com/templates/rt_solarsentinel_j15/images/header/green/logo.png'
     STREAMING_URL = 'rtmp://wow.eleden.com:1935/livesantander/livestream'
