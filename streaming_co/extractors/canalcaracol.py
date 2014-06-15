d0766a82c4dc5f4ce53f09cc398f6d922b139e3f
Ismaël Mejía <iemejia@gmail.com>
2014-06-16 01:43:50 +0200

    Re enabled direct link to canal caracol

--- streaming_co/extractors/canalcaracol.py
+++ streaming_co/extractors/canalcaracol.py
@@ -9,6 +9,6 @@ class CanalCaracolExtractor(StaticExtractor):
     NAME = u'Canal Caracol'
     WEBPAGE_URL = 'https://www.caracoltv.com/senal-vivo'
     LOGO_URL = 'http://st.caracoltv.co/sites/caracoltv.com/themes/caracol/images/programasProd/logo_big.png'
-    STREAMING_URL = 'rtmp://cp101307.live.edgefcs.net/live/V2djhrMTqxeN7cqjwSceDrDk7Im9z-x2_640_360_396@29268'
-    LIVESTREAMER_URL = 'hds://http://acaooyalahd2-lh.akamaihd.net/z/caracol01_delivery@187698/manifest.f4m?hdcore=2.10.3&g=PEWEWKTRRUJM'
-    IS_PLAYABLE = False
+    STREAMING_URL = "http://acaooyalahd2-lh.akamaihd.net/i/caracol04_delivery@187706/index_2128_av-b.m3u8?sd=10&rebase=on"
+    # LIVESTREAMER_URL = 'hds://http://acaooyalahd2-lh.akamaihd.net/z/caracol01_delivery@187698/manifest.f4m?hdcore=2.10.3&g=PEWEWKTRRUJM'
+    IS_PLAYABLE = True
